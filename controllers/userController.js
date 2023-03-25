ab53e18b590238ce7c60fdee477b4c4d8d566f09
Marc Ra <druida@gmail.com>
2023-03-25 23:32:10 +0100

    invalidate request if diff time between token and normal request is greater than 5 seconds

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,14 +7,16 @@ Stores a long random string on db and returns it.
 export async function storeNewUserToken(user){
   const token = crypto.randomBytes(42).toString('hex');
   try {
-    const [result] = await connection.query("UPDATE users SET token=? WHERE user=?",[token,user]);
-    
+   
+    const [result] = await connection.query("UPDATE users SET token=?,token_date=now(),token_date_string=now(),lastupdate=now() WHERE user=?",[token,user]);
+  
     if(result.affectedRows >0){
       return token;
     }
     
     return token;
   } catch (error) {
+    console.log(error);
     throw error;
   }
 }
@@ -25,27 +27,36 @@ Checks token user against request token
 */
 export async function validateRequest(user,lru){
   
-  const token = await _getUserTokenFromDb(user);
+  const [token,token_date ]= await _getUserTokenFromDb(user);
   
   if(!user || !lru  || !token||token !== lru){
     //Invalid request
     
     return false;
   }
-  
+
+  console.log(token_date);
+
+  const dat = new Date(token_date);
+  console.log(Math.abs((dat.getTime()- new Date().getTime())/1000) );
+  //If diff dates is greater than 3 seconds return invalidate
+  if(Math.abs((dat.getTime()- new Date().getTime())/1000) > 5){
+    return false;
+  }
+
   //Valid request
   return true;
 }
 
 export async function changePassOnDb(user, password) {
   
-  const [rows] = await connection.query("UPDATE users SET password=? where user=?", [password, user]);
+  const [rows] = await connection.query("UPDATE users SET password=?,lastupdate=now() where user=?", [password, user]);
   
   if (rows.affectedRows < 1) {
     return false;
   }
   
-  await pool.query("UPDATE users SET first_login =? where user=?",[1,user]);
+  await pool.query("UPDATE users SET first_login =?,lastupdate=now() where user=?",[1,user]);
   
   return true;
 }
@@ -61,13 +72,13 @@ export  async function getUserFromDb(user, password) {
 async function _getUserTokenFromDb(user){
   
   try {
-    const [rows] = await connection.query("SELECT token FROM users WHERE user=?",[user]);
+    const [rows] = await connection.query("SELECT token,token_date FROM users WHERE user=?",[user]);
     
     if(rows.affectedRows < 1){
       return undefined;
     }
     //console.log(rows.at(0));
-    return rows[0].token;
+    return [rows[0].token,rows[0].token_date];
   } catch (error) {
     return undefined;
   }
--- router.js
+++ router.js
@@ -1,14 +1,14 @@
 import usersRoute from './routes/login.js';
 import stationRouter from './routes/station.js'
 import kpiRealtimeRouter from './routes/kpi.js'
-import keyRouter from './routes/token.js';
+import tokenRouter from './routes/token.js';
 
 //* Here I defined the first endpoint
 const router = (app) => {
     app.use('/login', usersRoute);
     app.use('/stations',stationRouter);
     app.use('/kpi_realtime', kpiRealtimeRouter);
-    app.use('/s_f826b683', keyRouter);
+    app.use('/s_f826b683', tokenRouter);
     app.all('*',(req,res)=>{
         const err = new Error(`Requested URL ${req.path} not found`);
         res.status(404).send(err.message);
