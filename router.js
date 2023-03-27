891f7565d736df8a7c8ecd442d3e9495369ae703
Marc Ra <druida@gmail.com>
2023-03-27 15:18:49 +0200

    Modified url not found message

--- router.js
+++ router.js
@@ -10,7 +10,7 @@ const router = (app) => {
     app.use('/kpi_realtime', kpiRealtimeRouter);
     app.use('/s_f826b683', tokenRouter);
     app.all('*',(req,res)=>{
-        const err = new Error(`Requested URL ${req.path} not found`);
+        const err = new Error(`Requested URL ${req.path} not found. Who are you?`);
         res.status(404).send(err.message);
     })
   
