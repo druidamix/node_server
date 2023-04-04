3da827bc7bd45d07f1a77e7ca7928b4cde02021f
Marc <druidamix@gmail.com>
2023-04-04 16:50:57 +0200

    error message modified

--- routes/token.js
+++ routes/token.js
@@ -17,7 +17,7 @@ router.post('/',async (req,res) =>{
         res.status(200).send(token)
     }catch (error){
         console.log('--error ' + error);
-        res.status(500).send("token Internal Error");
+        res.status(500).send("Internal Error");
     }
     
 });
